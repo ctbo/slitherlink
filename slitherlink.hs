5a43127985000bb3e2d87883f1f633b76e8853c7
Harald Bögeholz <bo@ct.de>
2012-08-16 18:27:22 +0200

    primitve reading and showing of Problem

--- slitherlink.hs
+++ slitherlink.hs
@@ -1,10 +1,21 @@
+import Control.Monad.Instances
+
 data Constraint = Unconstrained | Exactly Int
 type Problem = [[Constraint]]
 
+sampleProblemString = unlines [".22.."
+                              ,"..13."
+                              ,"313.2"
+                              ,"....."
+                              ,".2.23"
+                              ]
 
 showConstraint :: Constraint -> Char
 showConstraint Unconstrained = '.'
 showConstraint (Exactly x) = head $ show x
+
+instance Show Constraint where
+    show = (:[]) . showConstraint         
 
 readConstraint :: Char -> Either String Constraint
 readConstraint '.' = Right Unconstrained
@@ -12,5 +23,7 @@ readConstraint '0' = Right $ Exactly 0
 readConstraint '1' = Right $ Exactly 1
 readConstraint '2' = Right $ Exactly 2
 readConstraint '3' = Right $ Exactly 3
+readConstraint c = Left $ "Invalid character " ++ show c ++ "."
 
-
+readProblem ::  String -> Either String Problem
+readProblem = (mapM . mapM) readConstraint . lines

32c3e65be27f50bfe25edd43769b524eb0b29a3c
Harald Bögeholz <bo@ct.de>
2012-10-15 23:42:06 +0200

    removed sample problem from solve.hs. Now displays a usage message when called without arguments.

--- solve.hs
+++ solve.hs
@@ -16,7 +16,7 @@ main = do
           [filename] -> do
                      s <- readFile filename
                      work s 2
-          [] -> work sampleProblemString 2
+          [] -> error "Usage: solve filename [#solutions]"
           _  -> error "Too many arguments."
   where work s n = case readProblem s of
              Left e -> putStrLn e
@@ -28,23 +28,3 @@ main = do
                          | otherwise = take n solutions
                    putStr $ concatMap showState display
                    putStrLn $ "Total number of solutions: " ++ show (length solutions)
-
-sampleProblemString :: String
-sampleProblemString = unlines [".3.112.2.."
-                              ,".3..3.1312"
-                              ,"22.1......"
-                              ,".3..3..2.2"
-                              ,"2.....2.21"
-                              ,"31.3.....3"
-                              ,"2.2..3..2."
-                              ,"......1.32"
-                              ,"2220.3..3."
-                              ,"..3.122.2."
-                              ]
-
--- stuff for interactive experiments
-
-sampleProblem :: Problem
-sampleProblem = case readProblem sampleProblemString of 
-  Right x -> x
-  Left _ -> undefined -- can't happen
